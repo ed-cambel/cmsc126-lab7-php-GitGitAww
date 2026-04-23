56cb969e6d06d9773a4a49d6f9105dc47fc04633
thoenii <msgelvezon@up.edu.ph>
2026-04-23 15:59:16 +0800

    Update DBConnector.php
    changed username and password with xampp defaults

--- DBConnector.php
+++ DBConnector.php
@@ -1,15 +1,27 @@
 <?php
 
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
+// create connection
 $conn = new mysqli($servername, $username, $password);
 
+// connection check
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully!";
 
+// create database
+$sql = "CREATE DATABASE gitgitawDB";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+// close connection
 $conn->close();
 ?>
